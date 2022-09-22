156df0dcb73b40dd65480d8726e71d3a9ac899fd
vazeraja <68131084+vazeraja@users.noreply.github.com>
2022-09-21 22:34:13 -0400

    add assembly definition references

--- Runtime/WorldGraph.cs
+++ Runtime/WorldGraph.cs
@@ -2,7 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WorldGraph : MonoBehaviour
-{
-   
+namespace ThunderNut.WorldGraph.Core {
+
+    public class WorldGraph : MonoBehaviour {
+        
+        
+        
+    }
+
 }
