54b353169b6ca0622f33648466566b17a55e3078
vazeraja <68131084+vazeraja@users.noreply.github.com>
2022-10-27 02:22:26 -0400

    precommit

--- Editor/Extensions/TwoPaneView.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine.UIElements;
-
-namespace ThunderNut.WorldGraph.Editor {
-
-    public class TwoPaneView : TwoPaneSplitView {
-        public new class UxmlFactory : UxmlFactory<TwoPaneView, TwoPaneSplitView.UxmlTraits> { }
-    }
-
-}
